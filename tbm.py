e2ac24b08328f65244f213b24bade6447b34dc28
João Pedro <jppc9292@gmail.com>
2015-04-11 19:59:11 -0300

    correção
    correção da variável tbm para tmb

--- tbm.py
+++ tbm.py
@@ -3,4 +3,5 @@ def TMB(sexo,peso,altura,idade):
     if sexo == 'homem':
         tmb = 88.36+(13.4*peso)+(4.8*altura)-(5.7*idade)
     else:
-
+        tmb = 447.6+(9.2*peso)+(3.1*altura)-(4.3*idade)
+    return tmb
