0b83bbb590b40d4cbec44047165453e666e3f36a
João Pedro <jppc9292@gmail.com>
2015-04-16 09:32:44 -0300

    [:2]
    pega os 2 primeiros termos

--- dieta.py
+++ dieta.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 arquivo = open('usuario.csv','r')
-leitura = arquivo.readlines()
+leitura = arquivo.readlines()[:2]
 for linha in leitura:
     linhas=linha.strip().split(',')
-    for j in linhas:
-        palavras=j.strip().split(',')
+    x=0
+    while x<1:
+        cadastro=linhas[0]
+        x+=1
+        print(cadastro)
+        
+        for j in linhas:
+            palavras=j.strip().split(',')
+            print(palavras)
+    
