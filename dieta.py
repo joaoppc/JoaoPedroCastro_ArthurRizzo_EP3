3a94cfcd1b0667ebde429d548ae69357cf2c075c
João Pedro <jppc9292@gmail.com>
2015-04-22 17:51:10 -0300

    variáveis linha 1
    cria variaveis para peso, sexo, peso, etc...

--- dieta.py
+++ dieta.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 arquivo = open('usuario.csv','r')
 leitura1 = arquivo.readlines()[1:2]#pega somente os 2 primeiros termos
 for linha1 in leitura1:
@@ -13,28 +14,4 @@ for linha1 in leitura1:
         fator = linhas1[5]
         x+=1
         print(nome,idade,peso,sexo,altura,fator)
-        leitura2=arquivo.readlines()[3:10]
-for linha2 in leitura2:
-    linhas2=linha2.strip().split(',')
-    z=0    
-    while z<1:
-        
-<<<<<<< Updated upstream
-        data = linhas2[0]
-        alimento=linhas2[1]
-        quantidade=linhas2[3]
-        z+=1
-        print(linhas2)
-        
-        #for j in linhas:
-         #   palavras=j.strip().split(',')
-          #  print(palavras)
-=======
-        for j in linhas:
-            palavras=j.strip().split(',')
-            print(palavras)
-            """isso é só a ideia"""
->>>>>>> Stashed changes
-    #nome+dict()
-    #nome={fulano}:30,70,m,1.64.... 
-    #nome[1]= 70
+
