43c2835b8f99acf8c3c4be8d60174b989585f297
João Pedro <jppc9292@gmail.com>
2015-04-22 23:07:12 -0300

    comentários
    comentários sobre o que foi feito

--- principal.py
+++ principal.py
@@ -24,7 +24,7 @@ for linha1 in leitura1:
         tmb=TMB(sexo,peso,altura,idade)
         necessidade = Necessidade_Calorica(fator,tmb)
 arquivo4 = open('usuario.csv','r')
-leitura4 = arquivo4.readlines()[3:]
+leitura4 = arquivo4.readlines()[3:]#pega somente as linhas com alimentos, sem os nomes
 arquivo2 = open('alimentos.csv','r')
 leitura3=arquivo2.readlines()
 calorias = []
@@ -32,16 +32,18 @@ for linha4 in leitura4:
     linhas4=linha4.strip().split(',')
     datas = linhas4[0]
     alimentos = linhas4[1]
-    quantidade = (linhas4[2])
+    quantidade = (linhas4[2])#quantidade consumida
     lista = [datas,alimentos,quantidade]
-
+# a idéa nesse loop abaixo era fazer um dicionário, más acabei ultilizando um método de lista
     for c in leitura3:
         linhas3=c.strip().split(',')
-        key=linhas3[0]
+        key=linhas3[0]#
         val=[(linhas3[1]),(linhas3[2]),linhas3[3],linhas3[4],linhas3[5]]
-        if key==alimentos:
-            cal = float(quantidade)/float(val[0])*float(val[1])
-            calorias.append(cal)
+        if key==alimentos:#compara se os nomes dos alimentos do usuario.csv
+                          #é igual a lista de alimentos do alimentos.csv
+            cal = float(quantidade)/float(val[0])*float(val[1])#calculo das calorias de cadaalimento ingerido
+            calorias.append(cal)#cria uma lista com as calorias de cada alimento ingerido
 
 cal_total = calorias[0]+ calorias[1]+ calorias[2]+ calorias[3]+ calorias[4]+ calorias[5]+ calorias[6]+calorias[7]
+#acima, calcula quantas calorias foram ingeridas nos 2 dias
 print(cal_total)                         
