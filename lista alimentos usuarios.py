e783210597c34c552aab014c0abce9db0e04fbe9
Althur <lvrizzo@luiz-vicente-rizzos-macbook-pro-2.local>
2015-04-23 01:24:16 -0300

    Tentativas

--- lista alimentos usuarios.py
+++ lista alimentos usuarios.py
@@ -5,12 +5,18 @@ Created on Wed Apr 22 16:28:31 2015
 @author: João Pedro
 """
 from datetime import *
+dias=[]
 arquivo4 = open('usuario.csv','r')
 leitura4 = arquivo4.readlines()[3:10]#pega somente os 2 primeiros termos
 for linha4 in leitura4:
-    linhas4=linha4.strip().split(',')
-    datas = linhas4[0]
+    linhas4=linha4.strip().split(",")
+    datas = (linhas4[0])
     alimentos = linhas4[1]
     quantidade = linhas4[2]
     d = datetime.strptime(datas,"%d/%m/%Y")
-    print(d)  
+    #dias += datas tentativa de unificar os dias em uma lista
+    print(datas)
+    #print(d)  
+    
+#print(dias)
+    
